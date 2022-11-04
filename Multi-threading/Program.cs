857e21c4b17d3f2485c433441e0d288461fdfdc6
Aryan Gupta <Aryan.Gupta@oneadvanced.com>
2022-11-04 10:13:08 +0530

    comments updated

--- Multi-threading/Program.cs
+++ Multi-threading/Program.cs
@@ -8,15 +8,17 @@ using System.Threading.Tasks;
 namespace Multi_threading
 {
     internal class Program
-    {
+    {        
         static void Main(string[] args)
         {
+            // Creating objects to execute the thread
             Thread firstThread = new Thread(MethodForFirstThread);
 
             Thread secondThread = new Thread(MethodForSecondThread);
 
             Thread thirdThread = new Thread(MethodForThirdThread);
 
+            // execution of thread
             firstThread.Start();
             secondThread.Start();
             thirdThread.Start();
@@ -24,6 +26,7 @@ namespace Multi_threading
             Console.ReadKey();
         }
 
+        // Method to execute using First Thread
         public static void MethodForFirstThread()
         {
             for (int i = 0; i < 5; i++)
@@ -35,6 +38,7 @@ namespace Multi_threading
             }
         }
 
+        // Method to execute using Second Thread
         public static void MethodForSecondThread()
         {
             for (int j = 0; j < 5; j++)
@@ -46,6 +50,7 @@ namespace Multi_threading
             }
         }
 
+        // Method to execute using Third Thread
         public static void MethodForThirdThread()
         {
             for (int j = 0; j < 5; j++)
